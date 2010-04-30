e4b71b5881ca69e2d5a665233cb82085b993b1b6
Peter Berkenbosch <peter@pero-ict.nl>
2010-04-30 20:56:08 +0200

    only use thumb style, make the banners in correct format or override the resize configuration/

--- app/models/banner.rb
+++ app/models/banner.rb
@@ -1,5 +1,5 @@
 class Banner < ActiveRecord::Base
   acts_as_list
-  has_attached_file :image, :styles => { :banner => "750", :thumb => "100x100>" }
+  has_attached_file :image, :styles => {:thumb => "100x100>" }
   default_scope :order => "position"
 end
